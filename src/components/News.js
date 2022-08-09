8ac51a174549d57064b61b7ffa73b1ec91010f67
SR0sohan <srssohan360@gmail.com>
2022-08-09 11:28:29 +0600

    apiin

--- src/components/News.js
+++ src/components/News.js
@@ -2,24 +2,39 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+  articles = []
+  constructor(){
+    super();
+    this.state ={
+        articles: this.articles,
+        loading :false
+    }
+  }
+
+  async componentDidMount(){
+            let url ="https://newsapi.org/v2/top-headlines?country=us&apiKey=5a265a187335421581803ddec83c50c2"
+            let data = await fetch(url);
+            let parsedData =await data.json();
+            console.log(parsedData)
+            this.setState({articles: parsedData.articles});
+
+    }
   render() {
     return (
-      <div>
         <div className='container mt-3'>
             <h2>News Fetch -- Top trending news</h2>
+            
             <div className='row mt-3'>
-                <div className='col-md-4'>
-                    <NewsItem title="mytitle" description="mydesc"/>
-                </div>
-                <div className='col-md-4'>
-                    <NewsItem title="mytitle" description="mydesc"/>
-                </div>
-                <div className='col-md-4'>
-                    <NewsItem title="mytitle" description="mydesc"/>
-                </div>
+
+            {this.state.articles.map((element)=>{
+
+              return <div className='col-md-4 mt-3' key={element.url}>
+                        <NewsItem  title={element.title} description={element.description.slice(0, 45)} imageUrl={element.urlToImage} newsUrl ={element.url}/>
+                     </div>
+            })}
+                
             </div>
          </div>
-      </div>
     )
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title,description} = this.props;
+    let {title,description,imageUrl,newsUrl} = this.props;
     return (
       <div>
             <div className="card" style={{width: "18rem"}}>
-                <img src="https://cdn.cnn.com/cnnnext/dam/assets/220805185254-anne-heche-file-031222-super-tease.jpg" className="card-img-top" alt="..."/>
+                <img src={imageUrl} className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
-                    <p className="card-text">{description}</p>
-                    <a href="/News" className="btn btn-primary">Go somewhere</a>
+                    <p className="card-text">{description}...</p>
+                    <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read more</a>
                 </div>
             </div>
       </div>
